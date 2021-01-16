b7f97e62f50e1549f07b73f9584f99d206c92ee1
Napuu <santeri.kaariainen@iki.fi>
2021-01-16 14:29:10 +0000

    Add reprojection to Naturalearth

--- naturalearth/download-and-import.sh
+++ naturalearth/download-and-import.sh
@@ -2,6 +2,6 @@ mkdir temp
 cd temp
 wget -i ../files
 ls -a1 | grep zip | xargs -I @ unzip @
-ls -a1 | grep shp$ | xargs -I @ ogr2ogr -f "PostgreSQL" PG:"host=$PGHOST port=$PGPORT user=$PGUSER password=$PGPASSWORD dbname=$PGDATABASE" -nlt MULTIPOLYGON @
+ls -a1 | grep shp$ | OGR_ENABLE_PARTIAL_REPROJECTION=true xargs -I @ ogr2ogr -f "PostgreSQL" PG:"host=$PGHOST port=$PGPORT user=$PGUSER password=$PGPASSWORD dbname=$PGDATABASE" -t_srs EPSG:3857 -nlt MULTIPOLYGON @
 cd ..
 rm -r temp

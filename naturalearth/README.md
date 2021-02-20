bbcbfffc837d333e4f6740c2681bc34119005ffb
Napuu <santeri.kaariainen@iki.fi>
2021-02-20 14:56:17 +0000

    add schema option

--- naturalearth/README.md
+++ naturalearth/README.md
@@ -3,4 +3,4 @@
 Download land, lake and country polygons from Naturalearth + import them to Postgis. https://www.naturalearthdata.com/downloads/10m-physical-vectors/ https://www.naturalearthdata.com/downloads/10m-cultural-vectors/
 
 
-Set `PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD` before running `sh download-and-import.sh`
+Set `PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD, PGSCHEMA` before running `sh download-and-import.sh`
--- naturalearth/download-and-import.sh
+++ naturalearth/download-and-import.sh
@@ -1,7 +1,13 @@
+PGHOST="${PGHOST:-192.168.1.111}"
+PGPORT="${PGPORT:-5432}"
+PGSCHEMA="${PGSCHEMA:-naturalearth}"
+PGPASSWORD="${PGPASSWORD:-test}"
+PGDATABASE="${PGDATABASE:-test}"
+PGUSER="${PGUSER:-test}"
 mkdir temp
 cd temp
 wget -i ../files
 ls -a1 | grep zip | xargs -I @ unzip @
-ls -a1 | grep shp$ | OGR_ENABLE_PARTIAL_REPROJECTION=true xargs -I @ ogr2ogr -f "PostgreSQL" PG:"host=$PGHOST port=$PGPORT user=$PGUSER password=$PGPASSWORD dbname=$PGDATABASE" -t_srs EPSG:3857 -nlt MULTIPOLYGON @
+ls -a1 | grep shp$ | OGR_ENABLE_PARTIAL_REPROJECTION=true xargs -I @ ogr2ogr -f "PostgreSQL" PG:"host=$PGHOST port=$PGPORT user=$PGUSER password=$PGPASSWORD dbname=$PGDATABASE schemas=$PGSCHEMA" -t_srs EPSG:3857 -nlt MULTIPOLYGON @
 cd ..
 rm -r temp
